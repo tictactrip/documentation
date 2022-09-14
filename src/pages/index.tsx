e8b9b45aa497a5a69d37c97169db0582cc7042b5
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2022-09-14 18:37:43 +0200

    feat: tagline + button wording updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Tictactrip API Tutorial
+            API Tutorial
           </Link>
         </div>
       </div>
