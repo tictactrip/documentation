2e7ffb87c86b1494101fd1feed515c0d7ede6481
Swann <swann.giboureau@tictactrip.eu>
2022-09-14 18:01:22 +0200

    feat(layout): change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Documentation Tictactrip`}
+      description="Réservez trains, bus et leurs combinaisons">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
